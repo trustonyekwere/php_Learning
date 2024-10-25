852660692f684071b9d400654ab702cd52ef168b
Trust Onyekwere <trustonyekwere151@gmail.com>
2024-10-25 11:59:59 +0100

    Indexed arrays

--- index.php
+++ index.php
@@ -31,14 +31,27 @@ $my_age = 25;
 
 // echo round($number);
 
-// echo rand(1 , 10);
-$yes = true;
-$no = false;
+// echo rand(1 , 10); //echo random number
+// $yes = true;
+// $no = false;
 
-echo $yes;
-echo $no;
+// echo $yes;
+// echo $no;
 
-$my_name = null;
+// $my_name = null;
+
+
+$coretech_staff = ["Abdul", "Francis", "Sean", "Victor", "Collins", "NOVA"]; //to add array
+
+$coretech_staff[1] = "Ebube"; //to make changes to an array
+
+array_push($coretech_staff, "Favor"); //to add array to the list
+
+array_splice($coretech_staff, 2, 3); //to remove values
+
+print_r($coretech_staff); //output readable array to browser
+
+
 
 ?>
 
@@ -52,7 +65,25 @@ $my_name = null;
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body>
-    <h1>Hello World, my name is <?php echo  $my_name; ?></h1>
+    <div class="container">
+    <h1>Hello World, my name is <?php echo $coretech_staff[5] ?></h1>
     <h4>I am  <?php echo $my_age ?> years old</h4>
+    <br>
+    <br>
+        <h4>Below are the staff from the tech company Core-Tech</h4>
+        <br>
+        <br>
+        <ol class="collection">
+            <li class="collection-item"><?php echo $coretech_staff[0]?></li>
+            <li class="collection-item"><?php echo $coretech_staff[1]?></li>
+            <li class="collection-item"><?php echo $coretech_staff[2]?></li>
+            <li class="collection-item"><?php echo $coretech_staff[3]?></li>
+            <li class="collection-item"><?php echo $coretech_staff[4]?></li>
+            <li class="collection-item"><?php echo $coretech_staff[5]?></li>
+            <li class="collection-item"><?php echo $coretech_staff[6]?></li>
+        </ol>
+        <br>
+        <br>
+    </div>
 </body>
 </html>
