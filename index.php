c51cf827edebc9a8c9beade3d53e442f62540674
Trust Onyekwere <trustonyekwere151@gmail.com>
2024-10-25 10:58:31 +0100

    learnt how to perform arithmetic functions with integers

--- index.php
+++ index.php
@@ -9,4 +9,29 @@ echo $my_age;
 $my_age = 25;
 echo $my_age;
 
+echo 'my name is $my_name'. 'I am $my_age years old' . "<br>";
+echo "my name is $my_name. I am $my_age years old" . "<br>";
+
+//Pythagoras Theorem
+$b = 4;
+$c = 5;
+
+$pythagoras_theorem = sqrt($b**2 + $c**2);
+
+echo $pythagoras_theorem . "<br>";
+
+//C of a circle
+$r = 20;
+
+$c = 2 * pi() * $r;
+
+echo $c . "<br>";
+
+$number = "7.00005";
+
+echo round($number);
+
+echo rand(1 0 , 20);
+
 ?>
+.
