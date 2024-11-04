18d02663f5a2732d65208616752f94a77e7308d7
Trust Onyekwere <trustonyekwere151@gmail.com>
2024-11-04 14:45:39 +0100

    learned for_loop

--- stock sample.php
+++ stock sample.php
@@ -1,18 +1,23 @@
 <?php 
 
-$stock = 24;
+// $stock = 24;
 
-if ($stock >= 24) {
-    echo $stock = "stock is full";
-}
-elseif ($stock >= 10 ) {
-    echo $stock = "stock is okay";
-}
-elseif ($stock >= 1 ) {
-    echo $stock = "stock is nearly empty";
-}
-else {
-    echo $stock = "stock is empty";
+// if ($stock >= 24) {
+//     echo $stock = "stock is full";
+// }
+// elseif ($stock >= 10 ) {
+//     echo $stock = "stock is okay";
+// }
+// elseif ($stock >= 1 ) {
+//     echo $stock = "stock is nearly empty";
+// }
+// else {
+//     echo $stock = "stock is empty";
+// }
+
+for ($Palmer=500; $Palmer <= 1000; $Palmer+=50) {
+    echo $Palmer . '<br>';
 }
 
-?>
+
+?> 
