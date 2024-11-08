d2199ab4f2c50641bda0d51f5c70c48512e6ff99
Trust Onyekwere <trustonyekwere151@gmail.com>
2024-11-08 12:10:32 +0100

    made separate header and footer page

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <h6 class="center-align blue darken-3 white-text">
+        <br>
+        <br>
+        &copy; Copyright Trust Onyekwere
+        <br>
+        <br>
+    </h6>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fantasy</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <h2 class="blue darken-3 white-text center-align" >
+        <br>
+        <br>
+        FANTASY PREMIER LEAGUE
+        <br>
+        <br>
+    </h2>
