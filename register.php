d30e172a43d9a1ae2a48341c87731545b1dd4699
Trust Onyekwere <trustonyekwere151@gmail.com>
2024-11-19 08:04:00 +0100

    created register page

---
+++ register.php
@@ -0,0 +1,52 @@
+<?php
+
+// print_r($_GET);
+
+//Using the GET method
+
+    //Creating empty variables to match the font fields
+
+    $full_name = "";
+    $email = "";
+    $password = "";
+    $message = "";
+
+    //Check if register button is pressed.
+
+    //also made the message to show onlyh when register button is clicked.
+
+    if (isset ($_GET["register"])) {
+        $full_name = $_GET ["full_name"];
+        $email = $_GET ["email"];
+        $password = $_GET ["password"];
+        $message = "Congratulations $full_name, you've registered successfully.";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <h1>Register</h1>
+    <form action="./register.php" method="GET">
+        <label for="full_name">Full Name:</label>
+        <input type="text" name="full_name" id="full_name"><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email"><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password"><br><br>
+
+        <input type="submit" value="Register" name="register" id="register">
+    </form>
+
+    <h3><?php echo $message; ?></h3>
+
+</body>
+</html>
