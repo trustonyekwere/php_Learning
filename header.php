4904133c371b17003b51df3be9e20fd98b6137e6
Trust Onyekwere <trustonyekwere151@gmail.com>
2024-12-03 14:10:11 +0100

    edited header and footer file

--- header.php
+++ header.php
@@ -3,15 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fantasy</title>
+    <title>Recipes</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body>
-    <h2 class="blue darken-3 white-text center-align" >
+    <h2 style="margin-top: -10px;" class="blue darken-3 white-text center-align">
         <br>
+        <p>RECIPES</p>
         <br>
-        FANTASY PREMIER LEAGUE
-        <br>
-        <br>
-    </h2>
+        </h2>
